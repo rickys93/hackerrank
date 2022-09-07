e58cc5303afdc1e7e38a05a17908d392109564b3
Richard S <rich_swainson@hotmail.co.uk>
2022-09-07 16:42:07 +0100

    attempting change to bomberman

--- bomberMan.py
+++ bomberMan.py
@@ -8,23 +8,30 @@ def bomberMan(n, grid):
             ans.append(r)
         return ans
     else:
-        bombsPlanted = {0:set()}
-        for row, r in enumerate(grid):
-            for col in range(len(r)):
-                if grid[row][col] == 'O':
-                    bombsPlanted[0].add((row,col))
+        bombsPlanted = {0:grid}
+        # bombsExploded = {1:set()}
+        # for row, r in enumerate(grid):
+        #     for col in range(len(r)):
+        #         if grid[row][col] == 'O':
+        #             bombsPlanted[0].add((row,col))
+        standardGrid = []
+
+        for row in range(len(grid)):
+            standardGrid.append('.' * len(grid[0]))
+
+
+    
         
         for i in range(1, n+1):
-            bombsPlanted[i] = set()
+            
             if i % 2 == 0:
+                bombsPlanted[i] = standardGrid.copy()
                 for row, r in enumerate(grid):
                     for col in range(len(r)):
                         plantBombAt(row, col, i, grid, bombsPlanted)
                         
             elif i >= 3 and i % 2 == 1:
-
-
-                    explodeBombAt(row, col, i, grid, bombsPlanted)
+                explodeBombs(i, grid, bombsPlanted)
                     
 
     return grid
@@ -34,37 +41,50 @@ def bomberMan(n, grid):
 
 def plantBombAt(row, col, i, grid, bombsPlanted):
     if grid[row][col] == '.':
-        bombsPlanted[i].add((row,col))
+        # bombsPlanted[i].add((row,col))
+        bombsPlanted[i][row] = bombsPlanted[i][row][:col] + 'O' + bombsPlanted[i][row][col+1:]
 
         # changing . for O to indicate bomb placed
         grid[row] = grid[row][:col] + 'O' + grid[row][col+1:]
 
-        return grid, bombsPlanted
 
-def explodeBombAt(row, col, i, grid, bombsPlanted):
+def explodeBombs(i, grid, bombsPlanted):
     bombsExploding = bombsPlanted[i-3]
-    for row, col in bombsExploding:
-        if grid[row][col] != 'O': return
 
-        rows = len(grid)
-        cols = len(grid[0])
+    for row in range(len(bombsExploding)):
+        for col in range(len(bombsExploding[0])):
+            if bombsExploding[row][col] == 'O' and grid[row][col] == 'O':
+    # for row, col in bombsExploding:
+    #     if grid[row][col] != 'O': continue
 
-        directions = ((0,1),(1,0),(0,-1),(-1,0))
-        grid[row] = grid[row][:col] + '.' + grid[row][col+1:]
-        for dx, dy in directions:
-            cRow = row + dx
-            cCol = col + dy
-            if cRow <= rows - 1 and cRow >= 0 and cCol <= cols - 1 and cCol >= 0 and grid[cRow][cCol] == 'O' and (cRow,cCol) not in bombsExploding:
-                grid[cRow] = grid[cRow][:cCol] + '.' + grid[cRow][cCol+1:]
+                explodeBombAt(i, row, col, bombsPlanted)
+
+                rows = len(grid)
+                cols = len(grid[0])
+
+                directions = ((0,1),(1,0),(0,-1),(-1,0))
+
+                for dx, dy in directions:
+                    cRow = row + dx
+                    cCol = col + dy
+                    if cRow <= rows - 1 and cRow >= 0 and cCol <= cols - 1 and cCol >= 0 and grid[cRow][cCol] == 'O' and (cRow,cCol) not in bombsExploding:
+                        explodeBombAt(i, cRow, cCol, bombsPlanted)
+
+
                           
 
-                
+def explodeBombAt(i, row, col, bombsPlanted):      
+    grid[row] = grid[row][:col] + '.' + grid[row][col+1:]
+    bombsPlanted[i-1] = bombsPlanted[i-1][row][:col] + '.' + bombsPlanted[i-1][row][col+1:]
+    # if (row, col) in bombsPlanted[i-1]:
+    #     bombsPlanted[i-1].remove((row, col))     
 
-grid = [
-    '...',
-    '.O.',
-    '...'
-]
+# grid = [
+#     '.O..',
+#     'O...',
+#     '...O',
+#     '....'
+# ]
 # grid =[
 #     '.......', 
 #     '...O...', 
