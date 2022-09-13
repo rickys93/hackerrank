f998c3c1e1b54f903139b77b758104d6d70f5a2e
Richard S <rich_swainson@hotmail.co.uk>
2022-09-13 14:04:25 +0100

    grid search added

--- bomberMan.py
+++ bomberMan.py
@@ -1,5 +1,21 @@
+#Bomberman lives in a rectangular grid. Each cell in the grid either contains a bomb or nothing at all.
+
+#Each bomb can be planted in any cell of the grid but once planted, it will detonate after exactly 3 seconds. Once a bomb detonates, it's destroyed — along with anything in its four neighboring cells. This means that if a bomb detonates in cell , any valid cells  and  are cleared. If there is a bomb in a neighboring cell, the neighboring bomb is destroyed without detonating, so there's no chain reaction.
+
+#Bomberman is immune to bombs, so he can move freely throughout the grid. Here's what he does:
+
+#Initially, Bomberman arbitrarily plants bombs in some of the cells, the initial state.
+#After one second, Bomberman does nothing.
+#After one more second, Bomberman plants bombs in all cells without bombs, thus filling the whole grid with bombs. No bombs detonate at this point.
+#After one more second, any bombs planted exactly three seconds ago will detonate. Here, Bomberman stands back and observes.
+#Bomberman then repeats steps 3 and 4 indefinitely.
+#Note that during every second Bomberman plants bombs, the bombs are planted simultaneously (i.e., at the exact same moment), and any bombs planted at the same time will detonate at the same time.
+
+#Given the initial configuration of the grid with the locations of Bomberman's first batch of planted bombs, determine the state of the grid after  seconds.
+
 def bomberMan(n, grid):
-    # Write your code here
+
+    # if n is even, always grid full of bombs
     if n > 2 and n % 2 == 0:
         ans = []
         for row in grid:
@@ -7,18 +23,24 @@ def bomberMan(n, grid):
             ans.append(r)
         return ans
     else:
+        # store answers in dict
         ansDict = {}
 
+        # when n = 3, 7, 11... grid is always the same
+        # when n = 5, 9, 13... grid is also always the same 
+        # so we only need to find the first 5 states of the grid
         for i in range(0, 6):
             for row in range(len(grid)):
                 for col in range(len(grid[0])):
          
+                    # extra seconds passed, update the grid
                     if grid[row][col] == 'O':
                         turnRowColTo(row, col, grid, '3')
                     elif grid[row][col] == '3':
                         turnRowColTo(row, col, grid, '2')
                     elif grid[row][col] == '2':
                         turnRowColTo(row, col, grid, '1')
+                    # when bomb has 0 second left, explode them
                     elif grid[row][col] == '1':
                         turnRowColTo(row, col, grid, '.')
 
@@ -28,12 +50,13 @@ def bomberMan(n, grid):
                                 if grid[row][col] == '.':
                                     turnRowColTo(row, col, grid, '3')
 
-            
+            # store grid in correct format
             ansDict[i] = turnBackToO(grid)
 
         if n < 3:
             ans = ansDict[n]
         else:
+            # if n = 3, 7, 11... or n = 5, 9, 13... use n = 3 or 5
             if n % 4 == 3:
                 n = 3
             elif n % 4 == 1:
---
+++ theGridSearch.py
@@ -0,0 +1,46 @@
+#Given an array of strings of digits G, try to find the occurrence of a given pattern of digits P. 
+#In the grid and pattern arrays, each string represents a row in the grid
+
+
+from operator import index
+
+
+def gridSearch(G, P):
+    # assume that pattern is not in G
+    patternInG = False
+
+    for numRow, row in enumerate(G):
+        # start at beginning of row
+        indexPattern = 0
+
+        # if first pattern is in current row and pattern not found yet
+        # we need to make sure that we check each matching pattern in the row, not just the first one
+        while P[0] in row[indexPattern:] and not patternInG:
+
+            # we have found a match of the first row of P, now we assume pattern is in G until this current iteration is proved wrong 
+            patternInG = True
+
+            # find the index of the current pattern
+            indexPattern += row[indexPattern:].index(P[0])
+
+            # find out if the pattern is in each row below
+            for i in range(1, len(P)):
+                if indexPattern+len(P[i]) <= len(row):
+
+                    # here we find the integers below the current row and see if they match
+                    rowBelow = G[numRow+i][indexPattern:indexPattern+len(P[i])]
+                    if P[i] == rowBelow:
+                        continue
+                    else:
+                        # if one row of the pattern does not match, this current iteration does not match the pattern and we do our next search
+                        patternInG = False
+                        break
+            indexPattern += 1
+
+    return 'YES' if patternInG else 'NO'
+                        
+
+G = ['15967259','22452334','34967121']
+P = ['59','34','22']
+
+gridSearch(G, P)
